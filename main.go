d6a85f72ea4e52c1948b8aa73fc4771f22b0527f
Antoine GIRARD <antoine.girard@sapk.fr>
2018-08-21 00:17:39 +0200

    Move cmd folder

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 // Copyright © 2018 Antoine GIRARD <antoine.girard@sapk.fr>
 package main
 
-import "github.com/sapk/go-genesys/tool/cmd"
+import "github.com/sapk/go-genesys/cmd"
 
 func main() {
 	cmd.Execute()
