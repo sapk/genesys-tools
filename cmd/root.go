2fc32ff38b3142be3475905d775ee4c3cf30819c
Antoine GIRARD <sapk@users.noreply.github.com>
2019-11-27 14:29:25 +0100

    add GPLv3 header to help message

--- cmd/root.go
+++ cmd/root.go
@@ -54,6 +54,21 @@ func init() {
 	}
 	cobra.OnInitialize(initVerbose)
 	RootCmd.Long = RootCmd.Short + fmt.Sprintf("\nVersion: %s - Branch: %s - Commit: %s - BuildTime: %s\n\n", Version, Branch, Commit, BuildTime)
+	RootCmd.Long += `
+    Copyright (C) 2019  GIRARD Antoine <Antoine.GIRARD@sapk.fr>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.`
 	RootCmd.PersistentFlags().BoolVarP(&appVerbose, "verbose", "v", false, "Turn on verbose logging")
 	RootCmd.AddCommand(&cobra.Command{
 		Use:   "version",
